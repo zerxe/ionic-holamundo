da4c2b7a633c36227a7ca2fa3e898088cc188599
zerxe <amc92v@gmail.com>
2023-09-22 03:35:19 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ionic.holamundo',
+  appName: 'ionic-holamundo',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
